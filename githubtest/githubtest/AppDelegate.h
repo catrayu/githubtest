f0526b5c6490e812e8f0b115b9a899af76c39061
catrayu <catrayu@catrayu-macair.local>
2015-06-10 15:00:32 +0800

    init the project

---
+++ githubtest/githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by catrayu on 15-6-10.
+//  Copyright (c) 2015年 catrayu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest/githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by catrayu on 15-6-10.
+//  Copyright (c) 2015年 catrayu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
